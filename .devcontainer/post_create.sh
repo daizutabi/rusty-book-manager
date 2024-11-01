f53acfaa5fc8cbb7d4664cdff7f5e2af3a9f8788
Daizu <daizutabi@gmail.com>
2024-11-01 21:12:13 +0000

    Add devcontainer

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+mkdir -p ~/.config
+cp .devcontainer/starship.toml ~/.config
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+cargo install --force cargo-make
